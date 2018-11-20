b61704aef19185a7bf4eea1c592b174dd9219538
zarrar <zarrarmalik81@gmail.com>
2018-11-20 17:29:38 +0000

    CommandLineRunner with new Users added

--- Springboot-Project/src/main/java/com/myself/zarrar/springboot/jpa/UserCommandLineRunner.java
+++ Springboot-Project/src/main/java/com/myself/zarrar/springboot/jpa/UserCommandLineRunner.java
@@ -1,15 +1,23 @@
 package com.myself.zarrar.springboot.jpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserCommandLineRunner implements CommandLineRunner{
 
+	@Autowired
+	private UserRepository repository;
+
+	
 	@Override
 	public void run(String... arg0) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("UserCommandLineRunner");
+		repository.save(new User("Zarrar","Admin"));
+		repository.save(new User("Kathryn","Secretary"));
+		repository.save(new User("Ed","User"));
+		repository.save(new User("Jenny","User"));
 	}
 	
 
