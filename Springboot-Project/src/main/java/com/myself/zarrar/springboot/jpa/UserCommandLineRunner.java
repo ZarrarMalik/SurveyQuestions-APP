d5ef49250a5b4668f7d6c38e875fec9d0265bb85
zarrar <zarrarmalik81@gmail.com>
2018-11-20 18:58:06 +0000

    H2-console and adding findBy method to find admin

--- Springboot-Project/src/main/java/com/myself/zarrar/springboot/jpa/UserCommandLineRunner.java
+++ Springboot-Project/src/main/java/com/myself/zarrar/springboot/jpa/UserCommandLineRunner.java
@@ -1,5 +1,7 @@
 package com.myself.zarrar.springboot.jpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -7,6 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserCommandLineRunner implements CommandLineRunner{
 
+	private static final Logger log = LoggerFactory
+            .getLogger(UserCommandLineRunner.class);
+	
 	@Autowired
 	private UserRepository repository;
 
@@ -18,6 +23,27 @@ public class UserCommandLineRunner implements CommandLineRunner{
 		repository.save(new User("Kathryn","Secretary"));
 		repository.save(new User("Ed","User"));
 		repository.save(new User("Jenny","User"));
+		
+		log.info("-------------------------------");
+        log.info("Finding all users");
+        log.info("-------------------------------");
+		for (User user : repository.findAll()) {
+            log.info(user.toString());
+            
+        }
+		log.info("Admin users are.....");
+		log.info("____________________");
+		for (User user : repository.findByRole("Admin")) {
+			log.info(user.toString());
+		}
+		
+		 log.info("-------------------------------");
+	        log.info("Finding user with id 1");
+	        log.info("-------------------------------");
+	        User user = repository.findOne(1L);
+	        log.info(user.toString());
+
+		
 	}
 	
 
