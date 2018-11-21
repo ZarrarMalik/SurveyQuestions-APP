4561a699e145e103114237dcd83354ca9bc9c116
zarrar <zarrarmalik81@gmail.com>
2018-11-21 18:23:41 +0000

    Integration Tests and unit tests added

--- Springboot-Project/src/main/java/com/myself/zarrar/springboot/jpa/UserRestRepository.java
+++ Springboot-Project/src/main/java/com/myself/zarrar/springboot/jpa/UserRestRepository.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface UserRepository extends CrudRepository<User, Long> {
-	 List<User> findByRole(String description);
+@RepositoryRestResource(path="users", collectionResourceRel="users")
+public interface UserRestRepository extends PagingAndSortingRepository<User, Long> {
+	 List<User> findByRole(@Param("role") String role);
 
 }
-
-
